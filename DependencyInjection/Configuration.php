45d410651016ed0e7c132c4b7a4f7513ed91ab5d
dismaster <ruslan.bahlai@gmail.com>
2014-12-01 18:03:54 +0200

    fix configuration v.4

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Voltash\UploadBundle\DependencyInjection;
+namespace NFC\UploadBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
