8a840a37369d252cd9cd9b20aa1d409a7c328c12
dismaster <ruslan.bahlai@gmail.com>
2014-12-01 17:38:45 +0200

    fix configuration

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('file_upload');
+        $rootNode = $treeBuilder->root('nfc_upload');
 
         $rootNode
             ->children()
